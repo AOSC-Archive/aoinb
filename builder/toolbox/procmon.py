e444be0cfe2726415cd012b14dd60c112d544aaf
Dingyuan Wang <gumblex@aosc.io>
2020-05-08 22:51:38 +0800

    procmon don't use systemd

--- builder/toolbox/procmon.py
+++ builder/toolbox/procmon.py
@@ -5,50 +5,63 @@ import os
 import sys
 import time
 import subprocess
+import collections
 
+import psutil
 import osutil
 
+if sys.version_info < (3, 7):
+    time.time_ns = lambda x: int(time.time() * 1e9)
+    time.monotonic_ns = lambda x: int(time.monotonic() * 1e9)
 
-def systemd_show(unit):
-    val_map = {'[not set]': None, '[no data]': None, 'yes': True, 'no': False}
-    proc = subprocess.run(
-        ('systemctl', 'show', unit), capture_output=True, check=True)
-    result = {}
-    for line in proc.stdout.decode().splitlines():
-        name, value = line.split('=', 1)
-        result[name] = val_map.get(value, value)
-    return result
+
+class ProcessStats(collections.namedtuple(
+    'ProcessStats', ('memory', 'cpu_sys', 'cpu_user'))):
+    def __add__(self, other):
+        return type(self)(
+            self.memory + other.memory,
+            self.cpu_sys + other.cpu_sys,
+            self.cpu_user + other.cpu_user,
+        )
+
+
+def get_process_stats(proc):
+    try:
+        with proc.oneshot():
+            cpu_times = proc.cpu_times()
+            print(cpu_times)
+            mem_info = proc.memory_full_info()
+        return ProcessStats(
+            mem_info.uss,
+            int(cpu_times.system * 1e9),
+            int(cpu_times.user * 1e9)
+        )
+    except (psutil.NoSuchProcess, psutil.AccessDenied):
+        return None
+
+
+def get_all_process_stats(pid):
+    
+    try:
+        proc = psutil.Process(pid)
+        stats = get_process_stats(proc) or ProcessStats(0, 0, 0)
+        for subproc in proc.children(recursive=True):
+            result = get_process_stats(subproc)
+            if result:
+                stats += result
+    except (psutil.NoSuchProcess, psutil.AccessDenied):
+        return None
+    return stats
 
 
 class ManagedProcess:
-    wrapper_args = (
-        'systemd-run', '--remain-after-exit', '--same-dir', '--nice=1',
-        '--property=Type=exec',
-        '--property=CPUAccounting=on', '--property=MemoryAccounting=on',
-    )
-
     def __init__(self, args, logfile, cwd=None, sd_args=()):
         self.cwd = cwd or os.getcwd()
         self.logfile = os.path.abspath(logfile)
-        cmd_args = list(self.wrapper_args)
-        cmd_args.append('--property=StandardOutput=append:' + self.logfile)
-        cmd_args.append('--property=StandardError=append:' + self.logfile)
-        cmd_args.extend(sd_args)
-        cmd_args.append('--')
-        cmd_args.extend(args)
-        print('Starting')
-        try:
-            proc = subprocess.run(
-                cmd_args, capture_output=True, check=True, cwd=self.cwd)
-        except subprocess.CalledProcessError as ex:
-            print('error: %s' % ex.stdout)
-            print(ex.stderr)
-        print('Started: %s' % proc.stdout)
-        print('Started: %s' % proc.stderr)
-        self.unit = proc.stderr.strip().decode().rsplit(' ', 1)[-1]
-        print('Started: %s' % self.unit)
-        d = systemd_show(self.unit)
-        self.start_mtn = int(d.get('ExecMainStartTimestampMonotonic') or 0)
+        self.proc = subprocess.Popen(
+            args, cwd=self.cwd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+        )
+        self.start_mtn = time.monotonic_ns()
         self.max_mem = None
         self.cpu_time = None
         self.real_time = None
@@ -62,42 +75,46 @@ class ManagedProcess:
         return None
 
     def poll(self):
-        d = systemd_show(self.unit)
-        result = d.get('CPUUsageNSec')
-        if result:
-            self.cpu_time = int(result)
-        if d['SubState'] != 'running':
-            self.exit_code = d['ExecMainStatus']
-            stop_mtn = int(d.get('ExecMainExitTimestampMonotonic') or 0)
-            self.real_time = (stop_mtn - self.start_mtn) * 1000
+        poll_time = time.monotonic_ns()
+        self.exit_code = self.proc.poll()
+        if self.exit_code is not None:
+            self.real_time = (poll_time - self.start_mtn)
             self.cwd_size = osutil.directory_size(self.cwd)
-            subprocess.run(('systemctl', 'stop', self.unit), check=True)
             return self.exit_code
-        result = int(d.get('MemoryCurrent') or 0)
-        if (self.max_mem or 0) < (result or 0):
-            self.max_mem = result
+        res = get_all_process_stats(self.proc.pid)
+        if not res:
+            return None
+        self.cpu_time = res.cpu_sys + res.cpu_user
+        if (self.max_mem or 0) < res.memory:
+            self.max_mem = res.memory
         return None
 
     def kill(self):
-        subprocess.run(('systemctl', 'kill', self.unit), check=True)
+        self.proc.terminate()
+        try:
+            self.proc.wait(1)
+        except subprocess.TimeoutExpired:
+            self.proc.kill()
+        self.poll()
 
     def wait(self):
-        with open(self.logfile, 'rb') as f:
+        with open(self.logfile, 'wb') as f:
             while self.poll() is None:
                 try:
-                    where = f.tell()
-                    while True:
-                        line = f.readline()
-                        if line:
-                            sys.stdout.buffer.write(line)
-                        else:
-                            break
+                    line = self.proc.stdout.readline()
+                    if line:
+                        sys.stdout.buffer.write(line)
+                        f.write(line)
+                    else:
+                        break
                     sys.stdout.buffer.flush()
-                    time.sleep(0.5)
-                    f.seek(where)
                 except KeyboardInterrupt:
                     self.kill()
+        self.poll()
         sys.stdout.buffer.flush()
+
+    def print_summary(self):
+        print('')
         print('='*20)
         print('Exit code: %s' % self.exit_code)
         if self.max_mem:
@@ -113,5 +130,6 @@ class ManagedProcess:
 
 
 if __name__ == '__main__':
-    p = ManagedProcess(sys.argv[1:], 'log.txt')
+    p = ManagedProcess(sys.argv[1:], 'log2.txt')
     p.wait()
+    p.print_summary()
